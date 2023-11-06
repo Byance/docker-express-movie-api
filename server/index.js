81f3625897fc599bc47fee3423db1b7043727798
Byance <53383935+Byance@users.noreply.github.com>
2023-11-06 20:24:50 -0300

    add sql container and consults working

--- server/index.js
+++ server/index.js
@@ -1,19 +1,18 @@
-import express from 'express'
-import { moviesRouter } from './routes/movies.js'
-import { corsMiddleware } from './middlewares/cors.js';
+import express from "express";
+import { moviesRouter } from "./routes/movies.js";
+import { corsMiddleware } from "./middlewares/cors.js";
 const app = express();
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 app.use(express.json());
-app.get('/', (req, res) => {
-    res.json({ "Title": 'Hola este es mi servidor hecho en express' });
+app.get("/", (req, res) => {
+  res.json({ Title: "Hola este es mi servidor hecho en express" });
 });
-app.use('/', (req, res, next)=> {
-    corsMiddleware(req, res)
-    next()
-})
-app.use('/movies', moviesRouter);
+app.use(corsMiddleware());
+app.use("/movies", moviesRouter);
 
 app.use((req, res) => {
-    res.status(404).send('<h1>404</h1>')
-})
-app.listen(process.env.PORT || 3000, () => console.log('Server is up and running and the reload work'));
+  res.status(404).send("<h1>404</h1>");
+});
+app.listen(process.env.PORT || 3000, () =>
+  console.log("Server is up and running and the reload work")
+);
--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,17 +1,18 @@
 import cors from "cors";
-const ACCEPTED_ORIGINS = ["http://localhost:8080"];
-export const corsMiddleware = (
-  req,
-  res,
-  { acceptedOrigins = ACCEPTED_ORIGINS }
-) => {
-  const origin = req.header("origin");
-  if (acceptedOrigins.includes(origin) || !origin) {
-    res.header("Access-Control-Allow-Origin", origin);
-  }
-};
 
-moviesRouter.options("/:id ", (req, res) => {
-  corsMiddleware(req, res);
-  res.header("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-});
+const ACCEPTED_ORIGINS = ["http://localhost:8080"];
+
+export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
+  cors({
+    origin: (origin, callback) => {
+      if (acceptedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  });
